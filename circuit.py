a250fa9c78f7161e999c43a1b61af795646664c3
Barış Özmen <hbaristr@gmail.com>
2025-07-04 11:31:21 +0800

    Update circuit.py

--- circuit.py
+++ circuit.py
@@ -11,17 +11,11 @@ def compute(encrypted_datasets: List[bytes], public_crypto_context: bytes) -> by
     vectors = [ts.bfv_vector_from(context=context, data=data) for data in encrypted_datasets]
     
     # multiplication depth: 1
-    num_alleles = len(vectors) * 2
     
     # Homomorphic sum
     encrypted_result = vectors[0]
     for vec in vectors[1:]:
         encrypted_result += vec
-
-    # Homomorphic division by constant
-    # Use multiplication by inverse since direct division is not supported
-    inverse = 1.0 / num_alleles
-    encrypted_result *= inverse
 
     # Serialize the result for return
     return encrypted_result.serialize()
